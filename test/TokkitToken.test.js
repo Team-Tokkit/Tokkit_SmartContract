ffc5cb31e097fb0e12f67c30d4311297c6244561
노영재 <146312456+YoungjaeRo@users.noreply.github.com>
2025-06-14 13:49:09 +0900

    Update TokkitToken.test.js

--- test/TokkitToken.test.js
+++ test/TokkitToken.test.js
@@ -14,34 +14,34 @@ describe("TokkitToken", function () {
     await token.waitForDeployment(); // v6+용
   });
 
-  it("✅ 배포자가 owner 여야 함", async function () {
+  it(" 배포자가 owner 여야 함", async function () {
     expect(await token.owner()).to.equal(owner.address);
   });
 
-  it("✅ owner가 mint 가능", async function () {
+  it(" owner가 mint 가능", async function () {
     await token.mint(addr1.address, 100);
     expect(await token.balanceOf(addr1.address)).to.equal(100);
   });
 
-  it("❌ owner가 아닌 사용자는 mint 불가", async function () {
+  it(" owner가 아닌 사용자는 mint 불가", async function () {
     await expect(token.connect(addr1).mint(addr2.address, 100))
       .to.be.revertedWithCustomError(token, "OwnableUnauthorizedAccount")
       .withArgs(addr1.address);
   });
 
-  it("✅ transfer 가능해야 함", async function () {
+  it(" transfer 가능해야 함", async function () {
     await token.mint(owner.address, 100);
     await token.transfer(addr1.address, 50);
     expect(await token.balanceOf(addr1.address)).to.equal(50);
   });
 
-  it("✅ owner가 burn 가능", async function () {
+  it(" owner가 burn 가능", async function () {
     await token.mint(addr1.address, 100);
     await token.burn(addr1.address, 50);
     expect(await token.balanceOf(addr1.address)).to.equal(50);
   });
 
-  it("❌ owner가 아닌 사용자는 burn 불가", async function () {
+  it(" owner가 아닌 사용자는 burn 불가", async function () {
     await token.mint(addr1.address, 100);
     await expect(token.connect(addr1).burn(addr1.address, 50))
       .to.be.revertedWithCustomError(token, "OwnableUnauthorizedAccount")
